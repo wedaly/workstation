5c7b0032807a94ffbd638f4c39bd8f1e45c1f903
Will Daly <will.e.daly@gmail.com>
2022-04-07 06:57:06 -0700

    upgrade chezmoi to latest version

--- bootstrap/fedora.sh
+++ bootstrap/fedora.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env sh
 
 echo "installing chezmoi"
-export CHEZMOI_VERSION=2.9.2
+export CHEZMOI_VERSION=2.15.0
 (cd /tmp && wget https://github.com/twpayne/chezmoi/releases/download/v${CHEZMOI_VERSION}/chezmoi-${CHEZMOI_VERSION}-x86_64.rpm)
 sudo dnf install /tmp/chezmoi-${CHEZMOI_VERSION}-x86_64.rpm
 
 echo "initializing chezmoi"
-chezmoi init https://github.com/wedaly/workstation && chezmoi apply
+chezmoi upgrade && chezmoi init https://github.com/wedaly/workstation && chezmoi apply
--- bootstrap/wsl-ubuntu.sh
+++ bootstrap/wsl-ubuntu.sh
@@ -17,8 +17,8 @@ apt update
 apt upgrade
 
 echo "installing chezmoi"
-export CHEZMOI_VERSION=2.9.2
+export CHEZMOI_VERSION=2.15.0
 (cd /tmp && wget https://github.com/twpayne/chezmoi/releases/download/v${CHEZMOI_VERSION}/chezmoi_${CHEZMOI_VERSION}_linux_amd64.deb && apt install ./chezmoi_${CHEZMOI_VERSION}_linux_amd64.deb)
 
 echo "initializing chezmoi"
-su $USERNAME --pty -c "chezmoi init https://github.com/wedaly/workstation && chezmoi apply"
+su $USERNAME --pty -c "chezmoi upgrade && chezmoi init https://github.com/wedaly/workstation && chezmoi apply"
