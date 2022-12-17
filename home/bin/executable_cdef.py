a12da353b6f71454def41c0a9c6a8501b6d2b9d3
Will Daly <will.e.daly@gmail.com>
2022-12-16 19:50:39 -0800

    tweak cdef.py

--- home/bin/executable_cdef.py
+++ home/bin/executable_cdef.py
@@ -41,18 +41,24 @@ def search_tags(dir, symbol):
     return [tag for tag in result.stdout.decode('utf-8').split("\n") if len(tag) > 0]
 
 
-PATTERN_RE = re.compile(r"\/\^(.+)\$?\/")
+PATTERN_RE = re.compile(r"\/(.+)\/")
 
 def tag_to_file_loc(dir, tag):
     # The tags(5) man page says that the pattern might also be a decimal line number
     # or a regex delimited by "?", but for now we just handle the /^ABC$/ regex form.
     symbol, path, pattern = tag.split('\t', maxsplit=2)
-    match = PATTERN_RE.match(pattern)
+    match = PATTERN_RE.search(pattern)
     if match is None:
         return None
 
     path = os.path.join(dir, path)
     snippet = match.group(1)
+
+    if snippet.startswith("^"):
+        snippet = snippet[1:]
+    if snippet.endswith("$"):
+        snippet = snippet[:-1]
+
     line = lookup_line_num(path, snippet)
     if line is None:
         return None
