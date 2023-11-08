ffc4fafc2c156dab7328ca5762d0582217b43cee
Will Daly <widaly@microsoft.com>
2023-11-08 13:11:04 -0800

    fix permission in Ubuntu bootstrap

--- bootstrap/ubuntu.sh
+++ bootstrap/ubuntu.sh
@@ -8,7 +8,7 @@ sudo apt upgrade
 
 echo "installing chezmoi"
 export CHEZMOI_VERSION=2.15.0
-(cd /tmp && wget https://github.com/twpayne/chezmoi/releases/download/v${CHEZMOI_VERSION}/chezmoi_${CHEZMOI_VERSION}_linux_amd64.deb && apt install ./chezmoi_${CHEZMOI_VERSION}_linux_amd64.deb)
+(cd /tmp && wget https://github.com/twpayne/chezmoi/releases/download/v${CHEZMOI_VERSION}/chezmoi_${CHEZMOI_VERSION}_linux_amd64.deb && sudo apt install ./chezmoi_${CHEZMOI_VERSION}_linux_amd64.deb)
 
 echo "initializing chezmoi"
 chezmoi upgrade && chezmoi init https://github.com/wedaly/workstation && chezmoi apply
